6936fa0d07ad041a7b7c0bb2a1787dbe8a4188ec
joycyan <joychen777@163.com>
2021-01-27 15:38:11 +0000

    LC1579 修改

--- src/JZOF/LC1579.java
+++ src/JZOF/LC1579.java
@@ -8,16 +8,106 @@ import java.util.Set;
 public class LC1579 {
     public static int maxNumEdgesToRemove(int n, int[][] edges) {
         int res = 0;
-        Set<int[]> resList = new HashSet<>();
-        /*Set<Integer> allEdges=new HashSet<>();
+        UF uf1 = new UF(n);
+        UF uf2 = new UF(n);
+        for(int i=0;i<=edges.length-1;i++){
+            if(edges[i][0]==3){
+                int x=edges[i][1];
+                int y=edges[i][2];
+                boolean a = uf1.union(x, y);
+                boolean b = uf2.union(x, y);
+                if (!a) {
+                    res++;
+                }
+            }
+        }
+        for(int i=0;i<=edges.length-1;i++){
+            if(edges[i][0]==1){
+                int x=edges[i][1];
+                int y=edges[i][2];
+                if (!uf1.union(x, y)) {
+                    res++;
+                }
+            }
+            else if(edges[i][0]==2){
+                int x=edges[i][1];
+                int y=edges[i][2];
+                if (!uf2.union(x, y)) {
+                    res++;
+                }
+            }
+        }
+        if(uf1.numbers()>1||uf2.numbers()>1){
+            return -1;
+        }
+        return res;
+    }
+}
+
+class UF {
+    int n;
+    int[] parent;
+    int[] rank;
+
+    public UF(int n) {
+        this.n = n;
+        this.parent = new int[n + 1];
+        this.rank = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int findRoot(int x) {
+        int xR = x;
+        while (xR != parent[xR]) {
+            parent[xR] = parent[parent[xR]];
+            xR = parent[xR];
+        }
+        return xR;
+    }
+
+    public boolean union(int x, int y) {
+        int xr = findRoot(x);
+        int yr = findRoot(y);
+        if (xr == yr) {
+            return false;
+        } else {
+            if (rank[xr] > rank[yr]) {
+                parent[yr] = xr;
+            } else if (rank[xr] < rank[yr]) {
+                parent[xr] = yr;
+            } else {
+                parent[xr] = yr;
+                rank[yr]++;
+            }
+        }
+        return true;
+    }
+    public int numbers(){
+        int res=0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i){
+                res++;
+            }
+        }
+        return res;
+    }
+}
+
+    /*太丑了太丑了！！！！！
+    public static int maxNumEdgesToRemove(int n, int[][] edges) {
+        int res = 0;
+       Set<int[]> resList = new HashSet<>();
+        Set<Integer> allEdges=new HashSet<>();
         for(int i=0;i<=edges.length-1;i++){
             allEdges.add(edges[i][1]);
             allEdges.add(edges[i][2]);
-        }*/
+        }
         List<int[]> edges1 = category(edges, 1);
         List<int[]> edges2 = category(edges, 2);
         List<int[]> edges3 = category(edges, 3);
-        Set<Integer> aliceEdges = new HashSet<>();
+       Set<Integer> aliceEdges = new HashSet<>();
         for (int i = 0; i <= edges1.size() - 1; i++) {
             aliceEdges.add(edges1.get(i)[1]);
             aliceEdges.add(edges1.get(i)[2]);
@@ -40,28 +130,31 @@ public class LC1579 {
         //alice
         UF uf1 = new UF(n);
         UF uf2 = new UF(n);
-        for (int i = 0; i <=edges3.size() - 1; i++) {
+        for (int i = 0; i <= edges3.size() - 1; i++) {
             int x = edges3.get(i)[1];
             int y = edges3.get(i)[2];
-            boolean a=uf1.union(x, y);
-            boolean b=uf2.union(x, y);
+            boolean a = uf1.union(x, y);
+            boolean b = uf2.union(x, y);
             if (!a) {
                 res++;
             }
         }
-        for (int i = 0; i <=edges1.size() - 1; i++) {
+        for (int i = 0; i <= edges1.size() - 1; i++) {
             int x = edges1.get(i)[1];
             int y = edges1.get(i)[2];
-            if (uf1.union(x, y)==false) {
+            if (!uf1.union(x, y)) {
                 res++;
             }
         }
         for (int i = 0; i <= edges2.size() - 1; i++) {
             int x = edges2.get(i)[1];
             int y = edges2.get(i)[2];
-            if (uf2.union(x, y)==false) {
+            if (!uf2.union(x, y)) {
                 res++;
             }
+        }
+        if(uf1.numbers()>1||uf2.numbers()>1){
+            return -1;
         }
 
         return res;
@@ -120,4 +213,15 @@ class UF {
         }
         return true;
     }
+    public int numbers(){
+        int res=0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i){
+                res++;
+            }
+        }
+        return res;
+
+    }
 }
+*/
